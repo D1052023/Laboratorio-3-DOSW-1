ffa50aaf8b412f82e9e30182cadcf30972f86cce
Oscar Sanchez <oscar.sporras@mail.escuelaing.edu.co>
2025-08-28 14:09:11 -0500

    Parte 1 - Sanchez Oscar: Preparación del espacio de Trabajo

---
+++ src/main/java/edu/dosw/lab/Laboratorio_3_DOSW_1/Laboratorio3Dosw1Application.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.Laboratorio_3_DOSW_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio3Dosw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio3Dosw1Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/lab/Laboratorio_3_DOSW_1/Laboratorio3Dosw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.Laboratorio_3_DOSW_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio3Dosw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
